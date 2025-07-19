f3503d2d318f47a825c7fcb731b0db8d63656db9
Snehalgupta-07 <guptasnehal877@gmail.com>
2025-07-20 01:02:51 +0530

    Backend Deployment related Bug fix

--- skillcertify-backend/src/utils/firebaseServiceAccount.js
+++ skillcertify-backend/src/utils/firebaseServiceAccount.js
@@ -1,14 +1,5 @@
-import { readFileSync } from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 import admin from "firebase-admin";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Go up from: src/utils → src → root → config
-const serviceAccountPath = path.resolve(__dirname, "../../config/firebaseServiceAccountKey.json");
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
+import { serviceAccount } from "../../config/firebaseServiceAccountKey.js";
 
 if (!admin.apps.length) {
   admin.initializeApp({
